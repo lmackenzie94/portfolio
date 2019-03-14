b02f40c38283c96b5caadb1a5dcde8cc35a5d54b
Luke <mackenzieluke94@gmail.com>
2019-03-13 20:33:19 -0400

    added in typed.js

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 $(function () {
 
+    $('.landingTextBox .button').delay(7000).fadeIn();
+
     $('.hamburger').on('click', function () {
         $('.hamburger').toggleClass('active'),
             $('.line1').toggleClass('active'),
@@ -12,4 +14,29 @@ $(function () {
         speed: 600
     });
 
+   
+
+});
+
+// typed.js
+var typed = new Typed('#p1', {
+    strings: [
+        "Hey, I'm Luke"
+    ],
+    typeSpeed: 40,
+    startDelay: 1000
+});
+var typed = new Typed('#p2', {
+    strings: [
+        "I'm a Toronto-based Web Developer"
+    ],
+    typeSpeed: 40,
+    startDelay: 2500
+});
+var typed = new Typed('#p3', {
+    strings: [
+        "Thanks for stopping by!"
+    ],
+    typeSpeed: 40,
+    startDelay: 5000
 });
