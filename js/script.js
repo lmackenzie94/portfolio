5955b7ece64f1f0c16b709d308c61e7e9f6fd84e
Luke MacKenzie <luke.mackenzie@campjefferson.com>
2020-01-08 21:29:53 -0500

    testing netlify forms

--- js/script.js
+++ js/script.js
@@ -1,34 +1,42 @@
 $(function() {
-  $(".landingTextBox .button")
+  // $('#contact_me_by_fax_only').on('change', function() {
+  //   if ($(this).prop('checked') == true) {
+  //     $('#submit').prop('disabled', true);
+  //   } else {
+  //     $('#submit').prop('disabled', false);
+  //   }
+  // });
+
+  $('.landingTextBox .button')
     .delay(4500)
     .show(0);
 
-  $(".hamburger").on("click", function() {
-    $(".hamburger").toggleClass("active"),
-      $(".line1").toggleClass("active"),
-      $(".line2").toggleClass("active"),
-      $(".line3").toggleClass("active");
-    $(".headerNav").toggleClass("active");
+  $('.hamburger').on('click', function() {
+    $('.hamburger').toggleClass('active'),
+      $('.line1').toggleClass('active'),
+      $('.line2').toggleClass('active'),
+      $('.line3').toggleClass('active');
+    $('.headerNav').toggleClass('active');
   });
 
-  $("a").smoothScroll({
+  $('a').smoothScroll({
     speed: 1000
   });
 });
 
 // typed.js for the landing page
-var typed = new Typed("#p1", {
+var typed = new Typed('#p1', {
   strings: ["Hey, I'm Luke"],
   typeSpeed: 25,
   startDelay: 1000
 });
-var typed = new Typed("#p2", {
+var typed = new Typed('#p2', {
   strings: ["I'm a Toronto-based Web Developer"],
   typeSpeed: 25,
   startDelay: 2000
 });
-var typed = new Typed("#p3", {
-  strings: ["Thanks for stopping by!"],
+var typed = new Typed('#p3', {
+  strings: ['Thanks for stopping by!'],
   typeSpeed: 25,
   startDelay: 3500
 });
